2cc3c1468b3931520493acccccff906cc2124c10
Yusuf Erdem <yusuf@thundra.io>
2022-11-11 15:03:03 +0300

    test

--- custom_test.go
+++ custom_test.go
@@ -3,10 +3,12 @@ package jsoniter
 import (
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func TestCustom(t *testing.T) {
 	t.Run("Fail Test", func(t *testing.T) {
+		time.Sleep(2 * time.Second)
 		should := require.New(t)
 		should.Equal(1, 123)
 	})
