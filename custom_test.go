e83a239a75d2ae6cfc1bc087dbab5336c1ddf147
Yusuf Erdem <yusuf@thundra.io>
2022-12-12 14:12:47 +0300

    test

--- custom_test.go
+++ custom_test.go
@@ -10,7 +10,7 @@ import (
 func TestCustom(t *testing.T) {
 
 	t.Run("Fail Test", func(t *testing.T) {
-		time.Sleep(1 * time.Second)
+		time.Sleep(1500 * time.Millisecond)
 		fmt.Println("Custom Test Output")
 		should := require.New(t)
 		should.Equal(1, 123)
