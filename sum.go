0b495feec70b73ea9769209f377e5641de8485d9
erdogan-yesil <69790758+erdogan-yesil@users.noreply.github.com>
2023-02-09 19:21:13 +0300

    Update sum.go

--- sum.go
+++ sum.go
@@ -11,3 +11,16 @@ func divide() int {
 func multiply() int {
 	return 6 * 2
 }
+
+func sum2() int {
+	return 2 + 3
+}
+
+func divide2() int {
+	return 6 / 3
+}
+
+func multiply2() int {
+	return 6 * 2
+}
+
